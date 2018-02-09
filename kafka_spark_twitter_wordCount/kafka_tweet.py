a533cd8847eb2ce87fa7d040180da70b518fa55c
Vaibhavi Patel <vaibhavi1321.10@gmail.com>
2018-02-09 02:29:47 -0500

    Add files via upload

--- kafka_spark_twitter_wordCount/kafka_tweet.py
+++ kafka_spark_twitter_wordCount/kafka_tweet.py
@@ -1,1 +1,60 @@
+#This program is for setting kafka and twitter api configurations so that kafka can send messages to spark using twitter API and its access token 
+import json
+from kafka import SimpleProducer, KafkaClient
+import tweepy
+from ConfigParser import SafeConfigParser
 
+# Note: Some of the imports are external python libraries. They are installed on the current machine.
+# If you are running multinode cluster, you have to make sure that these libraries
+# and currect version of Python is installed on all the worker nodes.
+
+class TweeterStreamListener(tweepy.StreamListener):
+    """ A class to read the twitter stream and push it to Kafka"""
+
+    def __init__(self, api):
+        self.api = api
+        super(tweepy.StreamListener, self).__init__()
+        client = KafkaClient("localhost:9092")
+        self.producer = SimpleProducer(client, async = True,
+                          batch_send_every_n = 1000,
+                          batch_send_every_t = 10)
+
+    def on_status(self, status):
+        """ This method is called whenever new data arrives from live stream.
+        We asynchronously push this data to kafka queue"""
+        msg =  status.text.encode('utf-8')
+        try:
+            self.producer.send_messages(b'twitterstream', msg)
+        except Exception as e:
+            print(e)
+            return False
+        return True
+
+    def on_error(self, status_code):
+        print("Error received in kafka producer")
+        return True # Don't kill the stream
+
+    def on_timeout(self):
+        return True # Don't kill the stream
+
+if __name__ == '__main__':
+
+    # Read the credententials from 'twitter-app-credentials.txt' file
+    parser = SafeConfigParser()
+    parser.read('key.txt')
+    consumer_key = parser.get('twitter','ConsumerKey')
+    consumer_secret = parser.get('twitter','ConsumerSecret')
+    access_key = parser.get('twitter','AccessToken')
+    access_secret = parser.get('twitter','AccessTokenSecret')
+
+    # Create Auth object
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+    # Create stream and bind the listener to it
+    stream = tweepy.Stream(auth, listener = TweeterStreamListener(api))
+
+    #Custom Filter rules pull all traffic for those filters in real time.
+    #stream.filter(track = ['love', 'hate'], languages = ['en'])
+    stream.filter(languages = ['en'],track=['#Trump','Donald Trump','White House'])
